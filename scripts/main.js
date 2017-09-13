9fe5f83014b6a9b75f5e9f964ae659e4aed7dd4a
Kevin Springer <KevinUSCU@outlook.com>
2017-09-13 12:00:36 -0700

    Add JS getElementById practice

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,13 @@
 
 // document.getElementById
 // Find just the title
+let title = document.getElementById('title')
 // Change the text of the title
+title.innerHTML = "Choose Your Magical Ingredients"
 // Change the style of the title
+title.style.color = "orange"
 // Add a class to the title
+title.className += " princess-font"
 
 // document.querySelectorAll
 // Find all the list items
